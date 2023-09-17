9bb002adde2fd9c7751f62d3b1e39c58a98c93b3
danielomos <danielomosele@gmail.com>
2023-09-17 22:12:04 +0000

    Code Synced by DhiWise

---
+++ src/pages/Home1/index.jsx
@@ -0,0 +1,186 @@
+import React from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import { Button, Img, Input, Line, Text } from "components";
+import Footer from "components/Footer";
+import Header from "components/Header";
+
+const Home1Page = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="bg-white-A700 flex flex-col font-poppins gap-2.5 items-center justify-center mx-auto w-auto sm:w-full md:w-full">
+        <Header className="flex md:flex-col flex-row md:gap-10 gap-[150px] items-center justify-center max-w-[1124px] mx-auto md:px-5 py-2.5 w-full" />
+        <div className="flex md:flex-col flex-row font-nunito gap-12 items-center justify-center max-w-[1158px] mx-auto md:px-5 w-full">
+          <div className="flex md:flex-1 flex-col items-center justify-start w-[54%] md:w-full">
+            <div className="flex flex-col gap-12 items-center justify-center w-[615px] md:w-full">
+              <div className="flex flex-col items-center justify-center w-auto md:w-full">
+                <Text
+                  className="leading-[110.00%] md:text-5xl text-[75px] text-gray-900_01"
+                  size="txtNunitoExtraBold75"
+                >
+                  <span className="text-gray-900_01 font-nunito text-left font-extrabold">
+                    Gas prices{" "}
+                  </span>
+                  <span className="text-gray-900_01 font-nunito text-left font-normal">
+                    <>
+                      in your
+                      <br />
+                    </>
+                  </span>
+                  <span className="text-gray-900_01 font-nunito text-left font-extrabold">
+                    neighborhood{" "}
+                  </span>
+                  <span className="text-gray-900_01 font-nunito text-left font-normal">
+                    <>
+                      in
+                      <br />
+                    </>
+                  </span>
+                  <span className="text-gray-900_01 font-nunito text-left font-extrabold">
+                    <>
+                      less than a <br />
+                      minute
+                    </>
+                  </span>
+                </Text>
+              </div>
+              <div className="flex flex-col items-center justify-start w-[98%] md:w-full">
+                <div className="flex flex-col gap-10 items-center justify-start w-[600px] md:w-full">
+                  <Input
+                    name="searchaddressa"
+                    placeholder="Enter Your address"
+                    className="leading-[normal] lowercase p-0 placeholder:text-gray-600 text-left text-xl w-full"
+                    wrapClassName="border border-blue_gray-100 border-solid w-full"
+                    shape="round"
+                    color="white_A700"
+                    size="xs"
+                    variant="fill"
+                  ></Input>
+                  <Button
+                    className="common-pointer cursor-pointer font-bold leading-[normal] min-w-[235px] text-center text-lg"
+                    onClick={() =>
+                      navigate("/stations", {
+                        state: { address: "10 ikosi road, lagos" },
+                      })
+                    }
+                    shape="round"
+                    color="gray_900_01"
+                    size="xs"
+                    variant="fill"
+                  >
+                    Search Now
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Img
+            className="sm:flex-1 h-[500px] md:h-auto object-cover rounded-[15px] w-[500px] sm:w-full"
+            src="images/img_image2.png"
+            alt="imageTwo"
+          />
+        </div>
+        <div className="flex flex-col font-kumbhsans gap-5 h-[760px] md:h-auto items-center justify-center max-w-[1324px] mx-auto md:px-5 w-full">
+          <div className="h-20 relative w-[639px] md:w-full">
+            <div className="absolute bg-white-A700 h-14 inset-x-[0] mx-auto top-[6%] w-full"></div>
+            <Text
+              className="absolute h-full inset-[0] m-auto md:text-5xl text-[64px] text-blue_gray-800 text-center tracking-[-0.64px] w-max"
+              size="txtKumbhSansExtraBold64"
+            >
+              Our Story
+            </Text>
+          </div>
+          <div
+            className="flex md:flex-col flex-row gap-[47px] items-center justify-center max-w-[1323px] w-full"
+            id="block2"
+          >
+            <div className="flex sm:flex-1 flex-col items-center justify-start w-[388px] sm:w-full">
+              <div className="flex flex-col gap-[45px] items-center justify-start w-[388px] sm:w-full">
+                <div className="flex flex-col font-kumbhsans gap-[23px] items-center justify-start w-[388px] sm:w-full">
+                  <Text
+                    className="text-2xl md:text-[22px] text-gray-900_02 sm:text-xl tracking-[-0.72px] w-auto"
+                    size="txtKumbhSansBold24"
+                  >
+                    Who we are?
+                  </Text>
+                  <Text
+                    className="leading-[24.00px] max-w-[388px] md:max-w-full text-base text-blue_gray-700 text-center tracking-[-0.32px]"
+                    size="txtKumbhSansRegular16"
+                  >
+                    Placing an order for a gas refill should not be a challenge.
+                    Provide your location, choose a gas station and select your
+                    preferred payment option.
+                  </Text>
+                </div>
+                <a
+                  href="javascript:"
+                  className="text-gray-900_01 text-lg underline w-auto"
+                >
+                  <Text size="txtNunitoBold18">Learn More</Text>
+                </a>
+              </div>
+            </div>
+            <div className="bg-gray-50 flex md:flex-1 flex-col items-center justify-center p-8 sm:px-5 rounded-[40px] w-[35%] md:w-full">
+              <div className="flex flex-col gap-[45px] items-center justify-start my-[198px] w-[388px] sm:w-full">
+                <div className="flex flex-col font-kumbhsans gap-[23px] items-center justify-start w-[388px] sm:w-full">
+                  <Text
+                    className="text-2xl md:text-[22px] text-center text-gray-900_02 sm:text-xl tracking-[-0.72px] w-auto"
+                    size="txtKumbhSansBold24"
+                  >
+                    What we do?
+                  </Text>
+                  <Text
+                    className="leading-[24.00px] max-w-[388px] md:max-w-full text-base text-blue_gray-700 text-center tracking-[-0.32px]"
+                    size="txtKumbhSansRegular16"
+                  >
+                    Don’t make the trip to the gas station and back just to get
+                    a refill. We will take care of pick-up and delivery so you
+                    can do more with your time.
+                  </Text>
+                </div>
+                <a
+                  href="javascript:"
+                  className="text-deep_purple-A400 text-lg underline w-auto"
+                >
+                  <Text size="txtNunitoBold18DeeppurpleA400">Learn More</Text>
+                </a>
+              </div>
+            </div>
+            <div className="flex md:flex-1 flex-col items-center justify-start w-[30%] md:w-full">
+              <div className="flex flex-col gap-[45px] items-center justify-start w-[388px] sm:w-full">
+                <div className="flex flex-col font-kumbhsans gap-[23px] items-center justify-start w-[388px] sm:w-full">
+                  <Text
+                    className="text-2xl md:text-[22px] text-center text-gray-900_02 sm:text-xl tracking-[-0.72px] w-auto"
+                    size="txtKumbhSansBold24"
+                  >
+                    What we see?
+                  </Text>
+                  <Text
+                    className="leading-[24.00px] max-w-[388px] md:max-w-full text-base text-blue_gray-700 text-center tracking-[-0.32px]"
+                    size="txtKumbhSansRegular16"
+                  >
+                    Worried that your gas refill won’t last long? Use our smart
+                    meter to monitor your gas usage. We’ll be right there before
+                    you pause your adventure.
+                  </Text>
+                </div>
+                <a
+                  href="javascript:"
+                  className="text-gray-900_01 text-lg underline w-auto"
+                >
+                  <Text size="txtNunitoBold18">Learn More</Text>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer className="flex font-nunito gap-[51px] items-center justify-center px-5 w-full" />
+      </div>
+    </>
+  );
+};
+
+export default Home1Page;
---
+++ src/pages/Stations/index.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { getStationNearby } from "service/api";
+
+import { Button, Img, Input, Line, Text } from "components";
+import Footer from "components/Footer";
+import Header from "components/Header";
+
+const StationsPage = () => {
+  const [nearData, setNearData] = React.useState();
+  const location = useLocation();
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    callApi();
+  }, []);
+
+  function callApi() {
+    const req = { params: { request: location?.state?.address } };
+
+    getStationNearby(req)
+      .then((res) => {
+        setNearData(res?.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        navigate("/notfoundstation", {
+          state: { areaAddress: location?.state?.address },
+        });
+      });
+  }
+
+  return (
+    <>
+      <div className="bg-white-A700 flex flex-col font-poppins gap-[30px] items-center justify-center mx-auto w-auto sm:w-full md:w-full">
+        <Header className="flex md:flex-col flex-row md:gap-10 gap-[150px] items-center justify-center max-w-[1124px] mx-auto md:px-5 py-2.5 w-full" />
+        <div className="flex flex-col font-kumbhsans items-center justify-center md:px-5 w-auto md:w-full">
+          <Text
+            className="md:text-5xl text-[64px] text-blue_gray-800 text-center tracking-[-0.64px] w-auto"
+            size="txtKumbhSansExtraBold64"
+          >
+            Gas rates in your area:
+          </Text>
+        </div>
+        <div className="flex flex-col font-nunito items-center justify-start max-w-[1400px] mx-auto md:px-5 w-full">
+          <div className="md:gap-5 gap-[41px] grid sm:grid-cols-1 md:grid-cols-2 grid-cols-3 justify-center min-h-[auto] w-full">
+            {nearData?.payload?.map((nearElement, index) => {
+              return (
+                <React.Fragment key={`nearElement${index}`}>
+                  <div className="bg-gray-50 flex flex-1 flex-col items-start justify-start sm:px-5 px-[39px] py-[42px] rounded-[30px] w-full">
+                    <div className="flex flex-col md:gap-10 gap-[100px] h-[340px] md:h-auto items-center justify-center w-[361px]">
+                      <div className="flex flex-row gap-[26px] items-center justify-start w-[361px]">
+                        <div className="bg-white-A700 flex flex-col h-[111px] items-end justify-end p-[19px] rounded-[20px] w-[111px]">
+                          <Img
+                            className="h-[63px] md:h-auto mt-2 object-cover w-[63px]"
+                            alt="iconEleven"
+                            src="nearElement?.stationLogoUrl"
+                          />
+                        </div>
+                        <div className="flex flex-col items-start justify-center w-[219px]">
+                          <Text
+                            className="sm:text-4xl md:text-[38px] text-[40px] text-center text-gray-900_01 w-auto"
+                            size="txtNunitoBold40"
+                          >
+                            {nearElement?.gasPrice}
+                          </Text>
+                          <Text
+                            className="text-[22px] text-center text-gray-600 sm:text-lg md:text-xl w-auto"
+                            size="txtNunitoRegular22"
+                          >
+                            Current Selling Price
+                          </Text>
+                          <Text
+                            className="text-[22px] text-center text-gray-600 sm:text-lg md:text-xl w-auto"
+                            size="txtNunitoRegular22"
+                          >
+                            {nearElement?.distance}
+                          </Text>
+                        </div>
+                      </div>
+                      <div className="flex flex-col items-center justify-center w-[361px]">
+                        <Text
+                          className="common-pointer text-3xl sm:text-[26px] md:text-[28px] text-center text-gray-900_01 underline w-auto"
+                          size="txtNunitoBold30"
+                          onClick={() =>
+                            navigate("/stationdetails", {
+                              state: { stationId: nearElement?.id },
+                            })
+                          }
+                        >
+                          View Station
+                        </Text>
+                      </div>
+                    </div>
+                  </div>
+                </React.Fragment>
+              );
+            })}
+          </div>
+        </div>
+        <Footer className="flex font-nunito gap-[51px] items-center justify-center px-5 w-full" />
+      </div>
+    </>
+  );
+};
+
+export default StationsPage;
